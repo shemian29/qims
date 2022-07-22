d96eb17e008a9924eec468b840e4e43530b56235
shemian29 <ims.vault.alpha@gmail.com>
2022-07-22 17:25:27 -0500

    Modify FloquetSpectrum and drive functions so that one can obtain the spectrum for an arbitrary set of pulses given by the user

--- qims/Floquet/Spectrum.py
+++ qims/Floquet/Spectrum.py
@@ -82,20 +82,44 @@ def Kick_Z(t, args):
             2 * args['width'] ** 2))
     return sm
 
+def Pulse(t, args):
+    """
+
+    :param t:
+    :param args:
+    :return:
+    """
+
+    # return args['amplitude'] * (1 / (np.sqrt(2 * np.pi))) * np.e ** \
+    # (-((t - np.floor(t / args['period']) * args['period']) ** 2) / (
+    #         2 * args['width'] ** 2))
+    return args['amplitude'] * (1 / (np.sqrt(2 * np.pi))) * np.e ** \
+    (-((t - np.floor(t / args['period']) * args['period']) ** 2) / (
+            2 * args['width'] ** 2))
+
 def FloquetSpectrum(qb, q_prms, d_prms, nsteps):
     """
     :param prms:
     :param qb:
     :return:
     """
-    args = {'amplitude': d_prms[0], 'width': d_prms[1], 'period': d_prms[2],'U_total':d_prms[3]}
-    # print(args)
-    HFloquet = [(2 * np.pi) * qubit_hamiltonian(qb, q_prms), \
-                [sx, lambda t, args: Kick_X(t, args)],\
-                [sy, lambda t, args: Kick_Y(t, args)], \
-                [sz, lambda t, args: Kick_Z(t, args)]]
+    args = {'amplitude': d_prms[0], 'width': d_prms[1], 'period': d_prms[2],'U_ops':d_prms[3], 't_pulse':d_prms[4]}
+
+    HFloquet = [(2 * np.pi) * qubit_hamiltonian(qb, q_prms)]
+
+    for r in range(len(args['U_ops'])):
+        HFloquet.append([args['U_ops'][r],\
+                         lambda t, args: Pulse(t-args['t_pulse'][r],args)])
+
+    # HFloquet = [(2 * np.pi) * qubit_hamiltonian(qb, q_prms), \
+    #             [sx, lambda t, args: Kick_X(t, args)],\
+    #             [sy, lambda t, args: Kick_Y(t, args)], \
+    #             [sz, lambda t, args: Kick_Z(t, args)]]
     options = qt.Options()
     options.nsteps = nsteps
     return qt.floquet_modes(HFloquet, args['period'], args=args, options=options)
 
 
+def flat_dev(spectrum):
+
+    return np.sum(np.abs(np.diff(spectrum)))
