3696e2b0a2f9f2cc8e55e7bdc04904d09ae919da
shemian29 <ims.vault.alpha@gmail.com>
2022-07-27 20:09:13 -0500

    Edit FloquetSpectrum function to make it more general for arbitrary drives

--- qims/Floquet/Spectrum.py
+++ qims/Floquet/Spectrum.py
@@ -1,32 +1,29 @@
-
-import scqubits as scq
-import qutip as qt
 import numpy as np
+import qutip as qt
+import qims as qims
 
 sx = qt.sigmax()
 sy = qt.sigmay()
 sz = qt.sigmaz()
 
-for r in range(len(args['U_ops'])):
-    HFloquet.append([args['U_ops'][r], \
-                     lambda t, args: Pulse(t - args['t_pulse'][r], args)])
 
-def FloquetSpectrum(H0, V_drive, nsteps):
+def FloquetSpectrum(H0, V_drives, nsteps):
     """
-    :param prms:
-    :param qb:
+
+    :param H0: qutip static Hamiltonian
+    :param V_drives: list of dictionary containing type of drive operator, drive profile and drive parameters
+    :param nsteps: number of steps used in the qutip Floquet integrator
     :return:
     """
     args = {'amplitude': d_prms[0], 'width': d_prms[1], 'period': d_prms[2],'U_ops':d_prms[3], 't_pulse':d_prms[4]}
 
-    HFloquet = [(2 * np.pi) * qubit_hamiltonian(qb, q_prms)]
+    HFloquet = [H0]
+    for r in range(len(V_drives)):
+        HFloquet.append([V_drives[r]["operator"], \
+                         lambda t, args=args[r]: V_drives[r]["drive_profile"](t, args)])
 
 
 
-    # HFloquet = [(2 * np.pi) * qubit_hamiltonian(qb, q_prms), \
-    #             [sx, lambda t, args: Kick_X(t, args)],\
-    #             [sy, lambda t, args: Kick_Y(t, args)], \
-    #             [sz, lambda t, args: Kick_Z(t, args)]]
     options = qt.Options()
     options.nsteps = nsteps
     return qt.floquet_modes(HFloquet, args['period'], args=args, options=options)
