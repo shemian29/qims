556267026e2ee3486bc59a23e986793e99ae6635
shemian29 <ims.vault.alpha@gmail.com>
2022-07-27 20:33:02 -0500

    Refactor Floquet to QFloquet

--- qims/QFloquet/Spectrum.py
+++ qims/QFloquet/Spectrum.py
@@ -12,7 +12,7 @@ def FloquetSpectrum(H0, V_drives, nsteps):
 
     :param H0: qutip static Hamiltonian
     :param V_drives: list of dictionary containing type of drive operator, drive profile and drive parameters
-    :param nsteps: number of steps used in the qutip Floquet integrator
+    :param nsteps: number of steps used in the qutip QFloquet integrator
     :return:
     """
     args = {'amplitude': d_prms[0], 'width': d_prms[1], 'period': d_prms[2],'U_ops':d_prms[3], 't_pulse':d_prms[4]}
