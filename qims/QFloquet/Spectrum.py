abf93d4d6bb207c2b9237e63f966b64f062a10de
shemian29 <ims.vault.alpha@gmail.com>
2022-08-05 18:24:53 -0500

    Add capability to use a single parameter for a drive and interpret it as a the same parameter for all pulses of the drive. Change V_drives for args. Implement the time-dependent Hamiltonian in terms of pre-known sx, sy, sz operators (for now this then works only for qubits). Initiate writing FloquetSpectrum_Sweep.

--- qims/QFloquet/Spectrum.py
+++ qims/QFloquet/Spectrum.py
@@ -7,28 +7,46 @@ sy = qt.sigmay()
 sz = qt.sigmaz()
 
 
-def FloquetSpectrum(H0, V_drives, nsteps):
+def FloquetSpectrum(H0, args, nsteps):
     """
 
-    :param H0: qutip static Hamiltonian
-    :param V_drives: list of dictionary containing type of drive operator, drive profile and drive parameters
+    :param H0: function that produces qutip static Hamiltonian
+    :param args: list of dictionary containing type of drive operator, drive profile and drive parameters
     :param nsteps: number of steps used in the qutip QFloquet integrator
     :return:
     """
-    args = {'amplitude': d_prms[0], 'width': d_prms[1], 'period': d_prms[2],'U_ops':d_prms[3], 't_pulse':d_prms[4]}
+    # Extract position of driving operators in input sequence of operators
+    for op_types in args["op_data"]["op_types"]:
+        args[op_types[0]] = qims.extract_oploc(op_types[1], args)
 
-    HFloquet = [H0]
-    for r in range(len(V_drives)):
-        HFloquet.append([V_drives[r]["operator"], \
-                         lambda t, args=args[r]: V_drives[r]["drive_profile"](t, args)])
+    # If input of a given drive parameter is a single number then the assumption is that the parameter applies to all driving operators
+    for term in list(args["drive_prms"].keys()):
+        chk = type(args["drive_prms"][term])
+        if ( type(chk) != list):
+            args["drive_prms"][term] = args["drive_prms"][term] * np.ones(
+                len(args["op_data"]["op_sequence"]))
 
+    # Full time-dependent Hamiltonian
+    HFloquet = [H0(args["static_prms"]), [sx, qims.drive_sx],
+                [sy, qims.drive_sy],
+                [sz, qims.drive_sz]]
 
-
+    # Set number of integration steps
     options = qt.Options()
     options.nsteps = nsteps
+
+    # Return eigenstates and quasi-energies
     return qt.floquet_modes(HFloquet, args['period'], args=args, options=options)
 
 
-def flat_dev(spectrum):
+def FloquetSpectrum_Sweep(H0, args, nsteps, sweeps):
 
+
+    for st_sw in list(sweeps.keys()):
+        for dr_sw in sweeps["drive_sweep"]:
+            evecs, evals = FloquetSpectrum(H0, args, nsteps)
+
+
+
+def flat_dev(spectrum):
     return np.sum(np.abs(np.diff(spectrum)))
