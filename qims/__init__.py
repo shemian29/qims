0546006d9050cab05aaa37b0506b578f0e3dc177
shemian29 <ims.vault.alpha@gmail.com>
2022-08-07 15:35:04 -0500

    Update files after resolving conflicts

--- qims/__init__.py
+++ qims/__init__.py
@@ -5,7 +5,7 @@ from qims.QMB.qentanglement import ent_entropy, PartBasis
 from qims.QFloquet.Spectrum import FloquetSpectrum,flat_dev
 from qims.QFloquet.Drives import drive_sx, drive_sy, drive_sz, extract_oploc, dd_protocols
 from qims.temp.JJCircuits import JJCircuit
-from qims.QMB.qsymmetry import GenerateMomentumBasis,Hk, TransInd
+from qims.QMB.qsymmetry import GenerateMomentumBasis,Hk, TransInd, MomentumEigensystem
 
 from qims.temp.JJCircuits import JJCircuit
 from qims.sc_circuits.Systems import qubit_hamiltonian
