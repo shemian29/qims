9e12c02355cac7862a15cd8d21dbddb92f935e79
shemian29 <ims.vault.alpha@gmail.com>
2022-08-22 16:36:45 -0500

    Introduce "Gaussian" vs "Square" pulse options in the drive_sxyz functions

--- qims/QFloquet/Drives.py
+++ qims/QFloquet/Drives.py
@@ -29,34 +29,66 @@ def dd_protocols(choice):
 
 def drive_sx(t, prms):
     sm = 0
-    for tx in prms["sx"]:
-        drive_prms = prms["drive_prms"]
-        t0 = drive_prms["tlocs"][tx] * prms["period"]
-        sm = sm + drive_prms['amplitudes'][tx]* np.cos(drive_prms["frequencies"][tx]*t+drive_prms["phases"][tx]) * (1 / (np.sqrt(2 * np.pi))) \
-             * np.exp(
-            -((t - t0 - np.floor(t / prms['period']) * prms['period']) ** 2) / (2 * drive_prms['widths'][tx] ** 2))
+
+    if prms["type"] == "Gaussian":
+        for tx in prms["sx"]:
+            drive_prms = prms["drive_prms"]
+            t0 = drive_prms["tlocs"][tx] * prms["period"]
+            sm = sm + drive_prms['amplitudes'][tx]* np.cos(drive_prms["frequencies"][tx]*t+drive_prms["phases"][tx]) * (1 / (np.sqrt(2 * np.pi))) \
+                 * np.exp(
+                -((t - t0 - np.floor(t / prms['period']) * prms['period']) ** 2) / (2 * drive_prms['widths'][tx] ** 2))
+    elif prms["type"] == "Square":
+        for tx in prms["sx"]:
+            drive_prms = prms["drive_prms"]
+            t0 = drive_prms["tlocs"][tx] * prms["period"]
+            if (t0-drive_prms["widths"][tx]/2)<t<(t0+drive_prms["widths"][tx]/2):
+                sm = sm + drive_prms['amplitudes'][tx]* np.cos(drive_prms["frequencies"][tx]*t+drive_prms["phases"][tx])
+
 
     return sm
 
 def drive_sy(t, prms):
     sm = 0
-    for ty in prms["sy"]:
-        drive_prms = prms["drive_prms"]
-        t0 = drive_prms["tlocs"][ty] * prms["period"]
-        sm = sm + drive_prms['amplitudes'][ty]* np.cos(drive_prms["frequencies"][ty]*t+drive_prms["phases"][ty])  * (1 / (np.sqrt(2 * np.pi))) \
-             * np.exp(
-            -((t - t0 - np.floor(t / prms['period']) * prms['period']) ** 2) / (2 * drive_prms['widths'][ty] ** 2))
+    if prms["type"] == "Gaussian":
+        for ty in prms["sy"]:
+            drive_prms = prms["drive_prms"]
+            t0 = drive_prms["tlocs"][ty] * prms["period"]
+            sm = sm + drive_prms['amplitudes'][ty]* np.cos(drive_prms["frequencies"][ty]*t+drive_prms["phases"][ty])  * (1 / (np.sqrt(2 * np.pi))) \
+                 * np.exp(
+                -((t - t0 - np.floor(t / prms['period']) * prms['period']) ** 2) / (2 * drive_prms['widths'][ty] ** 2))
+    elif prms["type"] == "Square":
+        for ty in prms["sy"]:
+            drive_prms = prms["drive_prms"]
+            t0 = drive_prms["tlocs"][ty] * prms["period"]
+            if (t0 - drive_prms["widths"][ty] / 2) < t < (t0 + drive_prms["widths"][ty] / 2):
+                sm = sm + drive_prms['amplitudes'][ty] * np.cos(
+                    drive_prms["frequencies"][ty] * t + drive_prms["phases"][ty])
 
     return sm
 
 def drive_sz(t, prms):
     sm = 0
-    for tz in prms["sz"]:
-        drive_prms = prms["drive_prms"]
-        t0 = drive_prms["tlocs"][tz] * prms["period"]
-        sm = sm + drive_prms['amplitudes'][tz] * np.cos(drive_prms["frequencies"][tz]*t+drive_prms["phases"][tz]) * (1 / (np.sqrt(2 * np.pi))) \
-             * np.exp(
-            -((t - t0 - np.floor(t / prms['period']) * prms['period']) ** 2) / (2 * drive_prms['widths'][tz] ** 2))
+
+    if prms["type"] == "Gaussian":
+        for tz in prms["sz"]:
+            drive_prms = prms["drive_prms"]
+            t0 = drive_prms["tlocs"][tz] * prms["period"]
+            sm = sm + drive_prms['amplitudes'][tz] * np.cos(drive_prms["frequencies"][tz]*t+drive_prms["phases"][tz]) * (1 / (np.sqrt(2 * np.pi))) \
+                 * np.exp(
+                -((t - t0 - np.floor(t / prms['period']) * prms['period']) ** 2) / (2 * drive_prms['widths'][tz] ** 2))
+    elif prms["type"] == "Square":
+        for tz in prms["sz"]:
+            drive_prms = prms["drive_prms"]
+            t0 = drive_prms["tlocs"][tz] * prms["period"]
+            if (t0 - drive_prms["widths"][tz] / 2) < t < (t0 + drive_prms["widths"][tz] / 2):
+                sm = sm + drive_prms['amplitudes'][tz] * np.cos(
+                    drive_prms["frequencies"][tz] * t + drive_prms["phases"][tz])
+
+
 
     return sm
+
+
+
+
 
