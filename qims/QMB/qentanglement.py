37d8a4b9267188dfef8f8f3f0714ac5468768644
shemian29 <ims.vault.alpha@gmail.com>
2022-08-28 15:53:44 -0500

    Change ent_entropy function so that it uses SVD from scipy with options that make the overall calculaion more efficiently. Swap evaluation of sub-system size and eigenstate indicies. Include import of qutip and scipy.linalg

--- qims/QMB/qentanglement.py
+++ qims/QMB/qentanglement.py
@@ -1,6 +1,8 @@
 import qims as qims
 import numpy as np
+import qutip as qt
 from tqdm.notebook import tqdm
+import scipy.linalg
 
 def PartBasis(size):
     bsl = {}
@@ -9,7 +11,7 @@ def PartBasis(size):
         bsl[l], bsl_ind[l] = qims.basis(int(l), parallel = False, bc = "open")
     return bsl, bsl_ind
 
-def ent_entropy(Hilbert_k_dims, evecs, basis_ind, partial_basis, size):
+def ent_entropy(Hilbert_k_dims, evecs,U, basis_ind, partial_basis, size):
 
     k_list = np.arange(0, size) / size
 
@@ -26,24 +28,36 @@ def ent_entropy(Hilbert_k_dims, evecs, basis_ind, partial_basis, size):
                 if qims.idstates(tmp, size=size) != -1:
                     scan.append([indL, indR, basis_ind[tmp]])
         PsiInds[l] = scan
-
+        # if l==1:
+            # print(PsiInds[l])
 
     S = {}
     # print("test")
     print("Evaluate entanglement entropy for each momentum, eigenstate and sub-system size:")
     for K in tqdm(k_list):
-        for l in tqdm(range(1, size)):
-            for n in range(Hilbert_k_dims[K]):
+        Uk = qt.Qobj(U[K])
+
+        for n in tqdm(range(Hilbert_k_dims[K])):
+            OP = ((Uk * evecs[K, n]).full().T[0])
+
+            for l in range(1, size):
+
 
 
                 Psi = np.zeros((len(partial_basis[l]), len(partial_basis[size - l])), dtype=complex)
 
+
                 for inds in PsiInds[l]:
-                    Psi[inds[0], inds[1]] = evecs[K, n][inds[2]][0, 0]
+                    Psi[inds[0], inds[1]] = OP[inds[2]]#[0, 0]
 
-                u, s, vh = np.linalg.svd(Psi, full_matrices=True)
+                # u, s, vh = np.linalg.svd(Psi, full_matrices=True)
+                s = 10 ** (-20)+ scipy.linalg.svd(Psi,
+                                                    full_matrices=False,
+                                                    compute_uv=False,
+                                                    check_finite=False,
+                                                    overwrite_a=True
+                                                    )
 
-                s = s + 10 ** (-20)
 
 
                 S[K, n, l] = -np.dot(s ** 2, np.log(s ** 2))
