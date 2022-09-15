59cc6b8372ea28fb2c580b42c6039e48a94a299a
shemian29 <ims.vault.alpha@gmail.com>
2022-09-14 20:45:47 -0500

    Create function that calculates the temporal correlation function SytSy

--- qims/QMB/qdynamics.py
+++ qims/QMB/qdynamics.py
@@ -3,18 +3,30 @@ import numpy as np
 import qims as qims
 from tqdm.notebook import tqdm
 
-sol = {}
+def SytSy(Sy, bs, Nx, Ukevecs, evals):
+    Corr3 = {}
+    Corrrand = {}
+    wscar = 1
+    wmax = 5 * wscar
+    dw = wscar / 20
+    tmax = (2 * wmax / (2 * wmax + dw)) * (2 * np.pi / (dw))
+    dt = 2 * np.pi / (2 * wmax + dw)
+    tlist = np.linspace(0, tmax, int(tmax / dt) + 1)
+    k_list = np.arange(0, Nx) / Nx
+    wlist = np.linspace(-wmax, wmax, len(tlist))
 
-H = qop.pxp_hamiltonian(bs ,bs_ind ,12)
-SZ = qop.sz_neel(size = 12, basis = bs)
+    itmax = 500
+    tmp = [qt.rand_ket(len(bs)) for n in range(itmax)]
+    sts_rand = qims.npqt2qtqt(tmp)
 
-wscar = 2* 0.636
-wmax = 3 * wscar
-dw = wscar / 20
-tmax = (2 * wmax / (2 * wmax + dw)) * (2 * np.pi / (dw))
-dt = 2 * np.pi / (2 * wmax + dw)
-t_list = np.linspace(0, tmax, int(tmax / dt))
-print(dt)
+    for k in tqdm(k_list[0:int(Nx / 2)]):
+        kit = ((k * Nx + int(Nx / 2)) % Nx) / Nx
+
+        aux_k = qims.npqt2qtqt(Ukevecs[k])
+        aux_kp = qims.npqt2qtqt(Ukevecs[kit])
+        Skkp = aux_k.dag() * Sy * aux_kp
+        Skkpaux_k = Skkp.dag() * aux_k.dag()
+        Skkpaux_kp = Skkp * aux_kp.dag()
 
 for n in tqdm(range(len(bs[nx]))):
     ProdSt = qt.basis(len(bs[nx]), n)
