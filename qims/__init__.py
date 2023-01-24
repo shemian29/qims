024ef40df5ea79bfdf011d4e1bb50f7e069c5fad
shemian29 <ims.vault.alpha@gmail.com>
2023-01-23 23:20:38 -0600

    Import new GeneralizedSimulation class for multi-qubit system

--- qims/__init__.py
+++ qims/__init__.py
@@ -1,4 +1,5 @@
-from qims.QMB.qbasis import basis, GenerateBasis, idstates,ind2occ,ind2state,occ,state2ind, Towers, npqt2qtqt, error
+from qims.QMB.qgeneral import GeneralizedSimulation
+from qims.QMB.qbasis import basis, GenerateBasis, idstates, ind2occ, ind2state, occ, state2ind, Towers, npqt2qtqt, error
 from qims.QMB.qoperators import constr, pxp, pxp_hamiltonian, pxp_operators, sp, sz_neel
 from qims.QMB.qentanglement import ent_entropy, PartBasis
 from qims.QMB.qdynamics import SpinOp_t_SpinOp_0, SytSy_alt
@@ -9,7 +10,7 @@ from qims.QFloquet.DecoherenceRates import filter_coefficients, dephasing_rate,S
 from qims.QFloquet.Spectrum import FloquetSpectrum,flat_dev
 from qims.QFloquet.Drives import drive_sx, drive_sy, drive_sz, extract_oploc, dd_protocols
 from qims.temp.JJCircuits import JJCircuit
-from qims.QMB.qsymmetry import GenerateMomentumBasis,Hk, TransInd, MomentumEigensystem
+from qims.QMB.qsymmetry import GenerateMomentumBasis, Hk, TransInd, MomentumEigensystem
 
 from qims.temp.JJCircuits import JJCircuit
 from qims.QCircuits.Systems import qubit_hamiltonian
